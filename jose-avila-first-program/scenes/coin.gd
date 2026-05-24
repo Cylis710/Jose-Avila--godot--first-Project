c9cfafd1d74c4a9bb83041c975174d131bb534c3
jose avila <avilajose514@yahoo.com>
2026-05-24 15:16:25 -0700

    Initial commit

---
+++ jose-avila-first-program/scenes/coin.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+
+
+func _on_body_entered(body: CharacterBody2D) -> void:
+	print("moneys! +1")
+	queue_free()
---
+++ jose-avila-first-program/script/player.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+
+
+const SPEED = 150.0
+const JUMP_VELOCITY = -230.0
+const MAX_Jumps = 2
+var Jumps_left = MAX_Jumps
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	else:
+		Jumps_left = MAX_Jumps
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and Jumps_left > 0:
+		velocity.y = JUMP_VELOCITY
+		Jumps_left -= 1
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction != 0:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
