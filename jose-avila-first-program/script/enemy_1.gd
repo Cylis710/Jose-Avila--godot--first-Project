8eab7b8887bef33968455568eccf800363ab6891
jose avila <avilajose514@yahoo.com>
2026-05-27 20:36:24 -0700

    w3d3
    layered map

--- jose-avila-first-program/script/enemy_1.gd
+++ jose-avila-first-program/script/enemy_1.gd
@@ -5,7 +5,6 @@ var direction = 1
 @onready var ray_cast_2d: RayCast2D = $RayCast2D
 @onready var ray_cast_2d_2: RayCast2D = $RayCast2D2
 @onready var timer: Timer = $Area2D/Timer
-@onready var player: CharacterBody2D = $"."
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 
